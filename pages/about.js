ffdf1ab03abcec5265982c17111bbedc5bb1ad9f
Gobitgogo <34598000+Gobitgogo@users.noreply.github.com>
2022-05-08 08:26:28 +0300

    Add files via upload

--- pages/about.js
+++ pages/about.js
@@ -1,44 +1,42 @@
 function about() {
   return (
     <>
-      <div className=" mt-5 flex w-full flex-col sm:flex-row">
-        <div className="mr-5 w-full sm:w-3/6">
-          <div className="h-screen  w-full border bg-[url('../assets/pic5.jpg')]  bg-cover bg-center bg-no-repeat "></div>
-          <div className="h-screen w-full text-center font-serif">
-            <h1 className="mb-4 pt-20 text-5xl">About My Work</h1>
-            <h2 className="mb-2 text-2xl"> The Photoshoots</h2>
-            <p className="text-md p-10">
-              Want the perfect image for your invitations, or more time to
-              perfect your wedding day poses? I’m available to shoot you and
-              your husband or wife to be in a private photo shoot designed to
-              bring the romance of your relationship to life. Get in touch and
-              tell me about the kind of photos you’d like to have for your
-              wedding. We can then work together to create unforgettable moments
-              for your invitations or thank you cards. Available Packages: 1.
-              Mini Package (50 images) 2. Medium Package (100 of images) 3. Maxi
-              Package (150 of images) For more details, please contact me.
-            </p>
-          </div>
+      <div className=" mt-5 mr-5 flex w-full flex-col sm:flex-row ">
+        <div className="h-screen w-full  border bg-[url('../assets/pic5.jpg')] bg-cover  bg-center bg-no-repeat sm:w-3/6 "></div>
+        <div className="h-screen w-full text-center font-serif sm:w-3/6">
+          <h1 className="mb-4 pt-20 text-5xl">About My Work</h1>
+          <h2 className="mb-2 text-2xl"> The Photoshoots</h2>
+          <p className="text-md p-10">
+            Want the perfect image for your invitations, or more time to perfect
+            your wedding day poses? I’m available to shoot you and your husband
+            or wife to be in a private photo shoot designed to bring the romance
+            of your relationship to life. Get in touch and tell me about the
+            kind of photos you’d like to have for your wedding. We can then work
+            together to create unforgettable moments for your invitations or
+            thank you cards. Available Packages: 1. Mini Package (50 images) 2.
+            Medium Package (100 of images) 3. Maxi Package (150 of images) For
+            more details, please contact me.
+          </p>
         </div>
-        <div className="w-full sm:w-3/6 ">
-          <div className=" h-screen w-full text-center font-serif ">
-            <h1 className="mb-4 pt-20 text-5xl">About Me</h1>
-            <h2 className="mb-2 text-2xl"> In A Few Words</h2>
-            <p className="text-md p-10">
-              Hello, I’m Isla. I’ve been working as a professional wedding
-              photographer for almost a decade, and love creating lasting
-              memories of people’s special day. I’m skilled at being discrete
-              and creative during the day, while capturing you and your guests
-              looking their your best. I work in a way that suits you, and
-              capture images in the style you like the best. I’ll chat to you
-              extensively before your wedding, and make sure everything goes off
-              without a hitch while you get hitched. I also offer the
-              opportunity to carry out photoshoots before or after your wedding
-              day to create even more priceless memories.
-            </p>
-          </div>
-          <div className=" h-screen w-full bg-[url('../assets/pic6.jpg')]  bg-cover bg-center bg-no-repeat "></div>
+      </div>
+      <div className="mr-5 flex w-full flex-col-reverse sm:flex-row">
+        <div className=" h-screen w-full text-center  font-serif sm:w-3/6 ">
+          <h1 className="mb-4 pt-20 text-5xl">About Me</h1>
+          <h2 className="mb-2 text-2xl"> In A Few Words</h2>
+          <p className="text-md p-10">
+            Hello, I’m Isla. I’ve been working as a professional wedding
+            photographer for almost a decade, and love creating lasting memories
+            of people’s special day. I’m skilled at being discrete and creative
+            during the day, while capturing you and your guests looking their
+            your best. I work in a way that suits you, and capture images in the
+            style you like the best. I’ll chat to you extensively before your
+            wedding, and make sure everything goes off without a hitch while you
+            get hitched. I also offer the opportunity to carry out photoshoots
+            before or after your wedding day to create even more priceless
+            memories.
+          </p>
         </div>
+        <div className="h-screen  w-full bg-[url('../assets/pic6.jpg')] bg-cover  bg-center bg-no-repeat sm:w-3/6 "></div>
       </div>
     </>
   )
